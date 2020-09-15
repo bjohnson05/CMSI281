26c9b79bb293794d82172c1803920f6da1846ded
B.J. Johnson <bjohnson05@gmail.com>
2020-09-15 14:09:05 -0700

    Updated ArraysTest.java to clean code

--- Classwork/Arrays/ArraysTest.java
+++ Classwork/Arrays/ArraysTest.java
@@ -31,25 +31,32 @@ public class ArraysTest {
       int[] b = { 19, 23, 2, 3, 5, 7, 9, 11, 13, 17 };
       int[] c = { 17, 19, 23, 2, 3 };
       int[] d = { 17, 23, 29, 31 };
+      
       System.out.println( "\n  a: " + Arrays.toString( a ) );
       String s1 = Arrays.toString( a );
+      
       StringBuilder s1s1 = new StringBuilder( Arrays.toString( a ) + Arrays.toString( a ) );
       System.out.println( "  s1: " + s1 );
       System.out.println( "  s1s1: " + s1s1.toString() );
+      
       int bracket = s1s1.indexOf( "]" );
       System.out.println( "  bracket index: " + bracket );
       s1s1.replace( bracket, bracket + 2, ", " );
+      
       s1 = s1s1.substring( 1, s1s1.length() - 1 );
       System.out.println( "  s1s1: " + s1s1.toString() );
       System.out.println( "  s1: " + s1 );
+      
       String s2 = Arrays.toString( b );
       s2 = s2.substring( 1, s2.length() - 1 );
       System.out.println( "  s2: " + s2 );
       System.out.println( "  s2 is rotation of s1? expeting true, got: " + (s1.contains( s2 ) ? "true" : "false") + "\n" );
+      
       s2 = Arrays.toString( c );
       s2 = s2.substring( 1, s2.length() - 1 );
       System.out.println( "  s2: " + s2 );
       System.out.println( "  s2 is rotation of s1? expeting true, got: " + (s1.contains( s2 ) ? "true" : "false") + "\n" );
+      
       s2 = Arrays.toString( d );
       s2 = s2.substring( 1, s2.length() - 1 );
       System.out.println( "  s2: " + s2 );
