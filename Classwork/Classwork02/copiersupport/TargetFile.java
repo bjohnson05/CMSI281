b725953c8fa3e4a211908b86ec6163cbe1ef04c3
B.J. Johnson <bjohnson05@gmail.com>
2020-09-16 11:20:56 -0700

    added TargetFile.java

---
+++ Classwork/Classwork02/copiersupport/TargetFile.java
@@ -0,0 +1,25 @@
+/**
+ * File name: TargetFile.java
+ * Purpose  : write out a copy of the input file
+ */
+
+package copiersupport;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class TargetFile {
+
+   public TargetFile( String fileName, String fileContent ) {
+      BufferedWriter bw = null;
+      try {
+         bw = new BufferedWriter( new FileWriter( fileName ) );
+         bw.write( fileContent );
+         bw.close();
+      }
+      catch( IOException ioe ) {
+         System.out.println( "Error writing target file." );
+      }
+   }
+}
