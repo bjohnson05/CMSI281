dbeb8d4d816ebe6003bfe0968eb6d34ab1bac493
B.J. Johnson <cmsi.bj.lmu@gmail.com>
2020-12-03 16:58:07 -0800

    new versions of Graph.java and DFS.java
    refactored to use "vertex" rather than "node" and to do an extra dfs traversal

--- Classwork/classwork14/DepthFirstSearcher.java
+++ Classwork/classwork14/DepthFirstSearcher.java
@@ -7,74 +7,113 @@
  *    graph that is constructed with a user-entered number of nodes.  The
  *    graph is expected to be of the associated "graph" class.
  *
- *    Operation is recursive; at each level, all the nodes in the graph are
- *    checked for connectivity with the current node.  The first node that
- *    is connected is used as the root of the next recursion.  Nodes are
+ *    Operation is recursive; at each level, all the Vertexs in the graph are
+ *    checked for connectivity with the current Vertex.  The first Vertex that
+ *    is connected is used as the root of the next recursion.  Vertexs are
  *    implemented as an adjacency matrix, in a pretty standard method.
- *    There is a boolean array to keep track of which nodes have been visited.
+ *    There is a boolean array to keep track of which Vertexs have been visited.
  *    A number of methods for performing the operation are also available.
  *    All of these items are part of the Graph.java class which is associated.
  *    This class actually creates and connects the graph to be searched.  It
  *    performs all the initializations, then does the search and lists the
- *    nodes as they become the root at each level of the recursion.  It's
+ *    Vertexs as they become the root at each level of the recursion.  It's
  *    pretty basic, but it shows the idea.
  *
  */
 
 public class DepthFirstSearcher   {
 
-   private static final boolean DEBUG_ON = true;
+   private static final boolean DEBUG_ON = false;
+   private static final int TEST_GRAPH_SIZE = 9;
+   private static char [] output = new char[TEST_GRAPH_SIZE];
+   private static int index = 0;
 
-  // Do a depth first search on theGraph from a specified node.  The node that
+  // Do a depth first search on theGraph from a specified Vertex.  The Vertex that
   //  is passed as an argument is used as the root in the recursive search.
-   public static void dfs( Graph theGraph, char currentNode ) {
-     // Mark the current node ("me") as visited and print its name
-      theGraph.markVisited( currentNode );
-      System.out.println( "  ** Visited " + currentNode + " **" );
+   public static void dfs( Graph theGraph, char currentVertex ) {
+     // Mark the current Vertex ("me") as visited and print its name
+      theGraph.markVisited( currentVertex );
+      System.out.println( "  ** Visited " + currentVertex + " **" );
+      output[index++] = currentVertex;
 
-     // Try all the nodes in the graph in the following way:
-     //  if there is a node connected to me, if that node hasn't been visited
+     // Try all the Vertexs in the graph in the following way:
+     //  if there is a Vertex connected to me, if that Vertex hasn't been visited
      //  already, then mark it visited and do a [recursive] depth first search
-     //  on the sub-graph with that node as the root
-      for( char nextNode = 'A'; nextNode <= 'I'; nextNode ++ ) {
-         if( DEBUG_ON ) System.out.print( "  Checking " + nextNode + " >> " );
-         if( theGraph.areTwoNodesConnected( currentNode, nextNode ) ) {
-            if( DEBUG_ON ) System.out.println( "  Nodes " + currentNode + " and " + nextNode + " ARE connected." );
-            if( !theGraph.wasVisited( nextNode ) ) {
-               if( DEBUG_ON ) System.out.println( "Searching with " + nextNode + " as root of tree." );
-               dfs( theGraph, nextNode );   // <-- HERE'S THE RECURSION!!
+     //  on the sub-graph with that Vertex as the root
+      for( char nextVertex = 'A'; nextVertex <= 'I'; nextVertex ++ ) {
+         if( DEBUG_ON ) System.out.print( "  Checking " + nextVertex + " >> " );
+
+         if( theGraph.areTwoVertexsConnected( currentVertex, nextVertex ) ) {
+
+            if( DEBUG_ON ) System.out.println( "  Vertexs " + currentVertex + " and " + nextVertex + " ARE connected." );
+
+            if( !theGraph.wasVisited( nextVertex ) ) {
+
+               if( DEBUG_ON ) System.out.println( "Searching with " + nextVertex + " as root of tree." );
+
+               dfs( theGraph, nextVertex );   // <-- HERE'S THE RECURSION!!
+
             } else {
-               if( DEBUG_ON ) System.out.println( "    But node " + nextNode + " was already visited." );
+
+               if( DEBUG_ON ) System.out.println( "    But Vertex " + nextVertex + " was already visited." );
+
             }
          } else {
-            if( DEBUG_ON ) System.out.println( "  Nodes " + currentNode + " and " + nextNode + " are NOT connected." );
+
+            if( DEBUG_ON ) System.out.println( "  Vertexs " + currentVertex + " and " + nextVertex + " are NOT connected." );
+
          }
       }
    }
 
   /**
    *  MAIN
-   *   The main method declares a graph of 9 nodes, initializes them and their
-   *   connections, then performs a DFS on the graph just listing out the nodes
+   *   The main method declares a graph of 9 Vertexs, initializes them and their
+   *   connections, then performs a DFS on the graph just listing out the Vertexs
    *   as they are visited.  It also shows the checks
    */
    public static void main( String [] args )
    {
-     // Create a new graph wih 8 nodes, which will be named 'A' through 'I'
-      Graph theGraph = new Graph( 9 );
+     // Create a new graph wih 8 Vertexs, which will be named 'A' through 'I'
+      Graph theGraph = new Graph( TEST_GRAPH_SIZE );
 
-     // Create links between the nodes.
-      theGraph.linkTwoNodes( 'A', 'B' );
-      theGraph.linkTwoNodes( 'A', 'C' );
-      theGraph.linkTwoNodes( 'A', 'D' );
-      theGraph.linkTwoNodes( 'A', 'E' );
-      theGraph.linkTwoNodes( 'B', 'F' );
-      theGraph.linkTwoNodes( 'F', 'H' );
-      theGraph.linkTwoNodes( 'D', 'G' );
-      theGraph.linkTwoNodes( 'G', 'I' );
+     // Create links between the Vertexs.
+      theGraph.linkTwoVertexs( 'A', 'B' );
+      theGraph.linkTwoVertexs( 'A', 'C' );
+      theGraph.linkTwoVertexs( 'A', 'D' );
+      theGraph.linkTwoVertexs( 'A', 'E' );
+      theGraph.linkTwoVertexs( 'B', 'F' );
+      theGraph.linkTwoVertexs( 'F', 'H' );
+      theGraph.linkTwoVertexs( 'D', 'G' );
+      theGraph.linkTwoVertexs( 'G', 'I' );
 
      // Do a depth first search on theGraph starting from 'E'
       dfs( theGraph, 'A' );
+      System.out.print( "\n   Output list of vertices: " );
+      for( int i = 0; i < TEST_GRAPH_SIZE; i++ ) {
+        System.out.print( output[i] + " " );
+      }
+      System.out.println( );
+
+      System.out.println( "\n\n   Starting over again, same graph, from 'G'... " );
+      theGraph = new Graph( TEST_GRAPH_SIZE );
+
+     // Create links between the Vertexs.
+      theGraph.linkTwoVertexs( 'A', 'B' );
+      theGraph.linkTwoVertexs( 'A', 'C' );
+      theGraph.linkTwoVertexs( 'A', 'D' );
+      theGraph.linkTwoVertexs( 'A', 'E' );
+      theGraph.linkTwoVertexs( 'B', 'F' );
+      theGraph.linkTwoVertexs( 'F', 'H' );
+      theGraph.linkTwoVertexs( 'D', 'G' );
+      theGraph.linkTwoVertexs( 'G', 'I' );
+      index = 0;
+      dfs( theGraph, 'G' );
+      System.out.print( "\n   Output list of vertices: " );
+      for( int i = 0; i < TEST_GRAPH_SIZE; i++ ) {
+        System.out.print( output[i] + " " );
+      }
+      System.out.println( );
+
    }
 }
-
--- Classwork/classwork14/Graph.java
+++ Classwork/classwork14/Graph.java
@@ -7,21 +7,21 @@
  *                   to the DepthFirstSearcher class.
  *
  *  This graph is represented simply as an adjacency matrix array.  The
- *    array is dimensioned as "n-by-n" where "n" is the number of nodes
- *    in the graph.  The graph is initialized by marking two nodes as
+ *    array is dimensioned as "n-by-n" where "n" is the number of vertexs
+ *    in the graph.  The graph is initialized by marking two vertexs as
  *    connected in the adjacency matrix.
  *
  */
 
 public class Graph   {
    private int   [][] adjaMatrix; // The adjacency matrix represents the graph
-   private boolean [] visited;    // Element is true if node is visited
+   private boolean [] visited;    // Element is true if vertex is visited
 
   // Constructor for the graph; this just declares the arrays, then
   //  simply initializes the adjacency matrix to all zeroes
-   public Graph( int numNodes )   {
-      adjaMatrix = new int[numNodes][numNodes];
-      visited    = new boolean[numNodes];
+   public Graph( int numVertexs )   {
+      adjaMatrix = new int[numVertexs][numVertexs];
+      visited    = new boolean[numVertexs];
 
      // Initialize all adjacency elements to zero (just insurance since ints
      //  are automatically initialized to zero), and initialize the visited
@@ -34,39 +34,38 @@ public class Graph   {
       }
    }
 
-  // Mark two nodes as connected.  This is done by putting a "1" in
-  //  each of the cells in the adjacency matrix for the two connected nodes
-   public void link( int nodeName1, int nodeName2 )   {
-      adjaMatrix[nodeName1][nodeName2] = 1;
-      adjaMatrix[nodeName2][nodeName1] = 1;
+  // Mark two vertexs as connected.  This is done by putting a "1" in
+  //  each of the cells in the adjacency matrix for the two connected vertexs
+   public void link( int vertexName1, int vertexName2 )   {
+      adjaMatrix[vertexName1][vertexName2] = 1;
+      adjaMatrix[vertexName2][vertexName1] = 1;
    }
 
-  // Make a link between two nodes by calling the "link()" method with
-  //  their converted indices; note that we have to FIND each node's index
-   public void linkTwoNodes( char nodeOne, char nodeTwo )   {
-      link( convertToIndex(nodeOne), convertToIndex(nodeTwo) );
+  // Make a link between two vertexs by calling the "link()" method with
+  //  their converted indices; note that we have to FIND each vertex's index
+   public void linkTwoVertexs( char vertexOne, char vertexTwo )   {
+      link( convertToIndex(vertexOne), convertToIndex(vertexTwo) );
    }
 
-  // Check if this node has been visited already
-   public boolean wasVisited( char node )   {
-      int index = convertToIndex( node );   // Calculate the node index
+  // Check if this vertex has been visited already
+   public boolean wasVisited( char vertex )   {
+      int index = convertToIndex( vertex );   // Calculate the vertex index
       return( visited[index] );             // Return the array value
    }
 
-  // Mark a node as having been visited
-   public void markVisited( char node )   {
-      visited[convertToIndex( node )] = true;
+  // Mark a vertex as having been visited
+   public void markVisited( char vertex )   {
+      visited[convertToIndex( vertex )] = true;
    }
 
-  // Calculate a node's index based on its letter name (ASCII)
-   public int convertToIndex( char nodeName ) {
-      return( nodeName - 'A' );
+  // Calculate a vertex's index based on its letter name (ASCII)
+   public int convertToIndex( char vertexName ) {
+      return( vertexName - 'A' );
    }
 
-  // Check if two nodes are connected
-   public boolean areTwoNodesConnected( char nodeOne, char nodeTwo )   {
-      return( adjaMatrix[convertToIndex(nodeOne)]
-                        [convertToIndex(nodeTwo)] == 1 );
+  // Check if two vertexs are connected
+   public boolean areTwoVertexsConnected( char vertexOne, char vertexTwo )   {
+      return( adjaMatrix[convertToIndex(vertexOne)]
+                        [convertToIndex(vertexTwo)] == 1 );
    }
 }
-
